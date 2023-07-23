f6dbb7ad552cae7fec8a0488df3d6f79f00cf585
Daniel Florez <daflorez406@misena.edu.co>
2023-07-23 03:33:30 -0500

    se sube proyecto-5

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="home">
+        <h1 className="home-title">Bienvenido a To-Do App</h1>
+        <p>¡Comienza a organizar tus tareas!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
